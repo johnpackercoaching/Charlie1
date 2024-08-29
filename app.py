cbb02b6ebdc0821e2a41eb1c3801fcd2174deeec
johnpackercoaching <157168795+johnpackercoaching@users.noreply.github.com>
2024-08-28 19:26:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import os
 import streamlit as st
+import openai
+import time
+
+assistant_id = "asst_82OOegTJqHOoI1PmZSTvk8Sa"
+
+APi
 
 # Retrieve the secret from the environment variable
 super_quiet_value = os.getenv("SUPER_QUIET")
 
 # Display the value in Streamlit
 st.write(f"The secret value is: {super_quiet_value}")
+
+
